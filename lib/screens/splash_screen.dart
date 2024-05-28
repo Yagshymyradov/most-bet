5672c3d5e2e0c1431acbd586e0b1ad2f65b0e4b6
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2024-05-28 19:03:44 +0500

    settings && splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../components/linear_percent_indicator.dart';
+import '../theme/theme.dart';
+import '../utils/assets.dart';
+import '../utils/navigation.dart';
+import 'on_boarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late final animController = AnimationController(vsync: this);
+  Timer? timer;
+  double percent = 0;
+
+  void replaceWithRootScreen() {
+    replaceRootScreen(context, const OnBoardingScreen());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    animController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        replaceWithRootScreen();
+      }
+    });
+
+    durationSecond();
+
+    animController
+      ..duration = const Duration(seconds: 20)
+      ..forward();
+  }
+
+  @override
+  void dispose() {
+    animController.dispose();
+
+    super.dispose();
+  }
+
+  void durationSecond() {
+    Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) {
+        percent += 0.5;
+        setState(() {
+          //no-op
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const SizedBox(),
+          AppIcons.logoReviewers.svgPicture(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 100),
+            child: LinearPercentIndicator(
+              lineHeight: 6,
+              percent: percent,
+              animation: true,
+              animateFromLastPercent: true,
+              barRadius: const Radius.circular(10),
+              backgroundColor: AppColors.darkGreyColor,
+              progressColor: AppColors.darkOrangeColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  AppColors._();
+
+  static const darkGreyColor = Color.fromRGBO(174, 174, 174, 1);
+  static const darkOrangeColor = Color.fromRGBO(238, 89, 22, 1);
+}
