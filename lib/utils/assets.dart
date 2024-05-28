287b715277800c75faca42f71533be4973f0b715
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2024-05-28 19:03:44 +0500

    settings && splash screen

---
+++ lib/utils/assets.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+enum AppIcons {
+  logoReviewers('logo-reviewers');
+
+  final String path;
+
+  const AppIcons(this.path);
+
+  String get _svg => 'assets/icons/$path.svg';
+
+  String get _png => 'assets/icons/$path.png';
+
+  String get _jpg => 'assets/icons/$path.jpg';
+
+  Widget svgPicture({
+    double? height,
+    double? width,
+    Color? color,
+  }) =>
+      SvgPicture.asset(
+        _svg,
+        height: height,
+        width: width,
+        color: color,
+      );
+
+  Widget get pngPicture => Image.asset(_png);
+
+  Widget get jpgPicture => Image.asset(_jpg);
+}
