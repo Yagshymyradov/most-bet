4fc8f27bfbf87847c9bfe556ded680f0e920fe45
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2024-06-17 23:06:11 +0500

    url_launcher permission

--- lib/screens/workout_screen/profile.dart
+++ lib/screens/workout_screen/profile.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:app_review/app_review.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
