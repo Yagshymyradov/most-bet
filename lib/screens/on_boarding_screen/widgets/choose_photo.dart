589accb19b58e673e309d6cbd329003d8aa5a9a0
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2024-06-21 12:34:51 +0500

    scroll choose photos

--- lib/screens/on_boarding_screen/widgets/choose_photo.dart
+++ lib/screens/on_boarding_screen/widgets/choose_photo.dart
@@ -60,57 +60,70 @@ class _ChooseProfilePhotoState extends State<ChooseProfilePhoto> {
     final textTheme = context.textTheme;
     final l10n = context.l10n;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          const SizedBox(height: 104),
-          Text(
-            l10n.letsCreteProfile,
-            textAlign: TextAlign.center,
-            style: textTheme.titleLarge,
-          ),
-          Text(
-            l10n.chooseYourPhoto,
-            textAlign: TextAlign.center,
-            style: textTheme.headlineMedium,
-          ),
-          const SizedBox(height: 36),
-          Wrap(
-            spacing: 10,
-            runSpacing: 10,
-            alignment: WrapAlignment.center,
-            children: ProfilePhoto.values
-                .map(
-                  (e) => InkWell(
-                    borderRadius: BorderRadius.circular(100),
-                    onTap: () => onProfileImageTap(e),
-                    child: ProfileImage(
-                      image: e.asImage,
-                      backgroundColor: e.backgroundColor(profileImage == e),
+    return Stack(
+      children: [
+        ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+          children: [
+            const SizedBox(height: 104),
+            Text(
+              l10n.letsCreteProfile,
+              textAlign: TextAlign.center,
+              style: textTheme.titleLarge,
+            ),
+            Text(
+              l10n.chooseYourPhoto,
+              textAlign: TextAlign.center,
+              style: textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 36),
+            Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              alignment: WrapAlignment.center,
+              children: ProfilePhoto.values
+                  .map(
+                    (e) => InkWell(
+                      borderRadius: BorderRadius.circular(100),
+                      onTap: () => onProfileImageTap(e),
+                      child: ProfileImage(
+                        image: e.asImage,
+                        backgroundColor: e.backgroundColor(profileImage == e),
+                      ),
                     ),
+                  )
+                  .toList(growable: false),
+            ),
+          ],
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: DecoratedBox(
+            decoration: const BoxDecoration(
+              color: AppColors.whiteColor,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: ElevatedButton(
+                onPressed: whenTapNextButton,
+                style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(
+                    profileImage != null //
+                        ? null
+                        : AppColors.primaryColor.withOpacity(0.5),
                   ),
-                )
-                .toList(growable: false),
-          ),
-          const Spacer(),
-          ElevatedButton(
-            onPressed: whenTapNextButton,
-            style: ButtonStyle(
-              backgroundColor: MaterialStatePropertyAll(
-                profileImage != null //
-                    ? null
-                    : AppColors.primaryColor.withOpacity(0.5),
+                ),
+                child: Text(
+                  l10n.next,
+                  style: textTheme.titleSmall,
+                ),
               ),
             ),
-            child: Text(
-              l10n.next,
-              style: textTheme.titleSmall,
-            ),
           ),
-          const SizedBox(height: 65),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
