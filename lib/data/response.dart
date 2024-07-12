49811dea8d4b61ad574cb38a16952e652d834ad0
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2024-07-12 22:34:28 +0500

    config response

---
+++ lib/data/response.dart
@@ -0,0 +1,26 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'response.g.dart';
+
+@JsonSerializable()
+class ConfigResponse {
+  final String codeTech;
+  final String server1_0;
+  final bool isAllChangeURL;
+  final bool isDead;
+  final String lastDate;
+  final String url_link;
+  final String? token;
+
+  ConfigResponse({
+    required this.codeTech,
+    required this.server1_0,
+    required this.isAllChangeURL,
+    required this.isDead,
+    required this.lastDate,
+    required this.url_link,
+    required this.token,
+  });
+
+  factory ConfigResponse.fromJson(Map<String, dynamic> json) => _$ConfigResponseFromJson(json);
+}
---
+++ lib/data/response.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ConfigResponse _$ConfigResponseFromJson(Map<String, dynamic> json) =>
+    ConfigResponse(
+      codeTech: json['codeTech'] as String,
+      server1_0: json['server1_0'] as String,
+      isAllChangeURL: json['isAllChangeURL'] as bool,
+      isDead: json['isDead'] as bool,
+      lastDate: json['lastDate'] as String,
+      url_link: json['url_link'] as String,
+      token: json['token'] as String?,
+    );
+
+Map<String, dynamic> _$ConfigResponseToJson(ConfigResponse instance) =>
+    <String, dynamic>{
+      'codeTech': instance.codeTech,
+      'server1_0': instance.server1_0,
+      'isAllChangeURL': instance.isAllChangeURL,
+      'isDead': instance.isDead,
+      'lastDate': instance.lastDate,
+      'url_link': instance.url_link,
+      'token': instance.token,
+    };
